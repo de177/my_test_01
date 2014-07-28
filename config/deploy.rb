7bb335d1f0f0b071c09c7b6f1fd264ef47fb582a
dima <de177@mail.ru>
2014-07-28 17:20:28 +0400

    ss

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,13 +1,40 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+require "rvm/capistrano"
+require "bundler/capistrano"
+require 'capistrano_colors'
+load 'deploy/assets'
+
+set :rvm_type, :system
+set :application, "my_test_01"
+
+set :repository, "git@github.com/de177/my_test_01.git"
+
+set :deploy_to, "/var/www/my_test_01"
+
+set :scm, :git
+set :branch, "master"
+set :user, "deploy"
+set :use_sudo, false
+
+set :rails_env, "production"
+set :deploy_via, :copy
+
+set :keep_releases, 5
+role :web, "95.131.89.90"
+role :app, "95.131.89.90"
+role :db, "95.131.89.90", :primary => true
+
+
+
+#set :application, "set your application name here"
+#set :repository,  "set your repository location here"
 
 # set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+#role :web, "your web-server here"                          # Your HTTP server, Apache/etc
+#role :app, "your app-server here"                          # This may be the same as your `Web` server
+#role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
+#role :db,  "your slave db-server here"
 
 # if you want to clean up old releases on each deploy uncomment this:
 # after "deploy:restart", "deploy:cleanup"
